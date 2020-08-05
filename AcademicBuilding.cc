8d342bd67e34db5bbe048287115a178344c85f7f
j42kong <j42kong@student.cs.uwaterloo.ca>
2020-08-05 16:38:55 -0400

    squares 1st commit martin

---
+++ AcademicBuilding.cc
@@ -0,0 +1,38 @@
+#include "AcademicBuilding.h"
+
+AcademicBuilding::AcademicBuilding(const std::string name,
+        int location, std::shared_ptr<VisitStrategy> visitStrategy,
+        const std::string owner, int purchaseCost,
+        bool isMortgaged, const vector<int>& neighbourIndice,
+        int improvementCost, const vector<int>& improvementFee)
+    : Building(name, location, visitStrategy, owner, purchaseCost,
+            isMortgaged, neighbourIndice), improvementCost(improvementCost),
+            improvementFee(improvementFee), improvementLevel(0) {}
+
+int AcademicBuilding::getImprovementCost() const {
+    return improvementCost;
+}
+
+int AcademicBuilding::getImprovementLevel() const {
+    return improvementLevel;
+}
+
+void AcademicBuilding::setImprovementLevel(int improvementLevel) {
+    this->improvementLevel = improvementLevel;
+}
+
+bool AcademicBuilding::checkMonopoly() const {
+    return allNeighbourOwned();
+}
+
+int AcademicBuilding::getFee() const {
+    if (improvementLevel == 0) {
+        if (checkMonopoly()) {
+            return improvementFee[0] * 2;
+        else
+            return improvementFee[0];
+    } else {
+        return improvementFee[improvementLevel];
+    }
+}
+
