47ae79a7db51d864899a14520cecaacb0ce6196b
Xiaoyu Zhu <x276zhu@uwaterloo.ca>
2020-08-05 16:15:58 -0400

    view

--- Observer.h
+++ Observer.h
@@ -1,16 +1,12 @@
 #ifndef _OBSERVER_H_
 #define _OBSERVER_H_
 
-class Building;
-class AcademicBuilding;
-class Player;
+class Subject;
 
 class Observer {
   public:
     // Pass the Subject that called the notify method.
-    virtual void notify( Building & whoNotified ) = 0; 
-    virtual void notify( AcademicBuilding & whoNotified ) = 0; 
-    virtual void notify( Player & whoNotified ) = 0; 
+    virtual void notify( Subject & whoNotified ) = 0; 
 };
 
 #endif
--- Subject.cc
+++ Subject.cc
@@ -3,3 +3,6 @@
 
 void Subject::attach(Observer *o) { observers.emplace_back(o); }
 
+void Subject::notifyObservers() {
+    for (auto &ob : observers) ob->notify( *this );
+}
