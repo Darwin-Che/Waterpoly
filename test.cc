b293897f52cc3144e68caf5e74f16b155e1c714d
Xiaoyu Zhu <x276zhu@uwaterloo.ca>
2020-08-12 22:22:27 -0400

    test window

--- test.cc
+++ test.cc
@@ -1,26 +1,28 @@
-#include <vector>
 #include <iostream>
-#include <string>
-#include <sstream>
-#include <map>
-#include <memory>
-#include "TextView.h"
+#include "window.h"
+
 using namespace std;
 
-/*int main(){
-    TextView tv(10,10);
-    int i;
-    /*while(cin >> i){
-        vector<int> p=tv.get2Dlocation(i);
-        cout << p[0]<<","<<p[1]<<endl;
-    }
-    tv.addPlayer('A');
-    for (int i=0;i<40;i++){
-        tv.addSquare("hello world");
-    }
-    tv.drawBoard();
+int main() {
+  Xwindow w;
+
+  for (int i = Xwindow::White ; i <= Xwindow::Blue; i++) {
+    w.fillRectangle(50 * i, 200, 50, 250, i);
+  }
+
+  w.drawString(50, 50, "Hello!");
+
+  w.drawString(50, 100, "ABCD");
+
+  w.drawString(50, 150, "Hello!");
+
+  Xwindow w2(199, 199);
+  w2.drawString(50, 100, "ABCD");
+
+
+  //w.showAvailableFonts();
+
+  char c;
+  cin >> c;
     return 0;
-}*/
-
-
-
+}
