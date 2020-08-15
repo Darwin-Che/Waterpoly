20c10a3880bac0dcbbaf732bdc9ebb47a6a8b525
Zhaocheng Che <z4che@uwaterloo.ca>
2020-08-15 19:14:01 -0400

    SLC & Needle add testing mode

--- NeedlesHallStrategy.cc
+++ NeedlesHallStrategy.cc
@@ -6,21 +6,21 @@
 // Constructor: Set up the strategies based on the probabilities given in specification
 NeedlesHallStrategy::NeedlesHallStrategy()
 {
-    strategies.push_back(std::make_shared<MoneyStrategy>(-200));
-    strategies.push_back(std::make_shared<MoneyStrategy>(200));
+    strategies.push_back(std::make_shared<MoneyStrategy>(-200)); // 0
+    strategies.push_back(std::make_shared<MoneyStrategy>(200));  // 1
     for (int i = 1; i <= 2; ++i)
     {
-        strategies.push_back(std::make_shared<MoneyStrategy>(-100));
-        strategies.push_back(std::make_shared<MoneyStrategy>(100));
+        strategies.push_back(std::make_shared<MoneyStrategy>(-100)); // 2 4
+        strategies.push_back(std::make_shared<MoneyStrategy>(100));  // 3 5
     }
     for (int i = 1; i <= 3; ++i)
     {
-        strategies.push_back(std::make_shared<MoneyStrategy>(-50));
-        strategies.push_back(std::make_shared<MoneyStrategy>(50));
+        strategies.push_back(std::make_shared<MoneyStrategy>(-50)); // 6 8 10
+        strategies.push_back(std::make_shared<MoneyStrategy>(50));  // 7 9 11
     }
     for (int i = 1; i <= 6; ++i)
     {
-        strategies.push_back(std::make_shared<MoneyStrategy>(25));
+        strategies.push_back(std::make_shared<MoneyStrategy>(25)); // 12 - 17
     }
 }
 
@@ -44,5 +44,41 @@ void NeedlesHallStrategy::acceptVisitor(std::shared_ptr<Player> player,
 
     // Randomly pick a strategy from the vector and apply it to the player
     int randomResult = rand() % 18;
+
+    if (Dice::testing_mode)
+    {
+        out << "In testing mode: enter the desired result:" << std::endl;
+        out << "1: -200; \t 2: -100; \t 3: -50; \t 4: +25; \n";
+        out << "5: +50; \t 6: +100; \t 7: +200" << std::endl;
+        int ans = 0;
+        in >> ans;
+        switch (ans)
+        {
+        case 1:
+            randomResult = 0;
+            break;
+        case 2:
+            randomResult = 2;
+            break;
+        case 3:
+            randomResult = 6;
+            break;
+        case 4:
+            randomResult = 12;
+            break;
+        case 5:
+            randomResult = 7;
+            break;
+        case 6:
+            randomResult = 3;
+            break;
+        case 7:
+            randomResult = 1;
+            break;
+        default:
+            break;
+        }
+    }
+
     strategies[randomResult]->acceptVisitor(player, board, in, out);
 }
--- SLCStrategy.cc
+++ SLCStrategy.cc
@@ -8,15 +8,15 @@ SLCStrategy::SLCStrategy()
 {
     for (int i = 1; i <= 3; ++i)
     {
-        strategies.push_back(std::make_shared<MoveStrategy>(-3));
-        strategies.push_back(std::make_shared<MoveStrategy>(1));
+        strategies.push_back(std::make_shared<MoveStrategy>(-3)); // 0 2 4
+        strategies.push_back(std::make_shared<MoveStrategy>(1));  // 1 3 5
     }
     for (int i = 1; i <= 4; ++i)
     {
-        strategies.push_back(std::make_shared<MoveStrategy>(-2));
-        strategies.push_back(std::make_shared<MoveStrategy>(-1));
-        strategies.push_back(std::make_shared<MoveStrategy>(2));
-        strategies.push_back(std::make_shared<MoveStrategy>(3));
+        strategies.push_back(std::make_shared<MoveStrategy>(-2)); // 6 10 14 18
+        strategies.push_back(std::make_shared<MoveStrategy>(-1)); // 7 11 15 19
+        strategies.push_back(std::make_shared<MoveStrategy>(2));  // 8 12 16 20
+        strategies.push_back(std::make_shared<MoveStrategy>(3));  // 9 13 17 21
     }
 }
 
@@ -40,14 +40,54 @@ void SLCStrategy::acceptVisitor(std::shared_ptr<Player> player,
 
     int timsLocation = board->getSquareBuilding("DC Tims Line")->getLocation();
     int moveAmount = timsLocation - player->getPosition();
-    strategies.push_back(std::make_shared<MoveStrategy>(moveAmount));
+    strategies.push_back(std::make_shared<MoveStrategy>(moveAmount)); // 22
 
     int OSAPLocation = board->getSquareBuilding("COLLECT OSAP")->getLocation();
     moveAmount = OSAPLocation - player->getPosition();
-    strategies.push_back(std::make_shared<MoveStrategy>(moveAmount));
+    strategies.push_back(std::make_shared<MoveStrategy>(moveAmount)); // 23
 
     // Randomly pick a strategy from the vector and apply it to the player
     int randomResult = rand() % 24;
+
+    if (Dice::testing_mode)
+    {
+        out << "In testing Mode, please choose the following: " << std::endl;
+        out << "1: Back 3; \t 2: Back 2; \t 3: Back 1; \n";
+        out << "4: Forward 1; \t 5: Forward 2; \t 6: Forward 3; \n";
+        out << "7: Go to DC Tims Line \n";
+        out << "8: Advance to Collect OSAP" << std::endl;
+        int ans = 0;
+        in >> ans;
+        switch (ans)
+        {
+        case 1:
+            randomResult = 0;
+            break;
+        case 2:
+            randomResult = 6;
+            break;
+        case 3:
+            randomResult = 7;
+            break;
+        case 4:
+            randomResult = 1;
+            break;
+        case 5:
+            randomResult = 8;
+            break;
+        case 6:
+            randomResult = 9;
+            break;
+        case 7:
+            randomResult = 22;
+            break;
+        case 8:
+            randomResult = 23;
+            break;
+        default:
+            break;
+        }
+    }
 
     strategies[randomResult]->acceptVisitor(player, board, in, out);
     if (randomResult == 22) // Go To DC Tims Line is selected
